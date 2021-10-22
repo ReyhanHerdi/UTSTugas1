b736df4c0fe95d3984e733caaa611cbc0675e58b
Reyhan Herdiyanto <90316516+ReyhanHerdi@users.noreply.github.com>
2021-10-22 20:41:21 +0700

    Add files via upload

--- utstugas1.cpp
+++ utstugas1.cpp
@@ -5,42 +5,61 @@ using namespace std;
 /*
 	g++ utstugas1.cpp -o
 
-	yiyo
-
-	!!! INI MASIH BELUM SELESAI !!!
+	bisahore
 */
 
 	int main()
 	{
 		
-		int angka1, angka2, n, v ;
+		cout << "Kalkulator yang hanya bisa melakukan perkalian dan pembagian \n" << endl ;
+
+		int angka1, v, s, i ;
+		float angka2, hasil ;
 		char op, kp ;
 
-
-
-		cout << "a = " ;
-		cin >> angka1 ;
-		cout << "b = " ;
-		cin >> angka2 ;
-		cout << "operator: " ;
-		cin >> op ;
-
-		if (op == '*')
+		do
 		{
-			do
-			{
-				cout << angka1 << "*" << angka2 << " = " << v << endl ;
-				v += angka1 ;		
+
+			cout << "a = " ;
+			cin >> angka1 ;
+			cout << "b = " ;
+			cin >>angka2 ;
+			cout << "Operator: " ;
+			cin >> op ;
+
+				if (op == '*')
+				{
+					hasil = 0 ; // biar ngitungnya mulai dari nol lagi
+					for (int i = 0; i < angka2; i++) // i = buat pengulangannya
+					{
+						hasil += angka1 ; // Nanti berarti hasil + angka 1 sampai berapa i
+					}
+					cout << angka1 << " * " << angka2 << " = " << hasil << endl ;
+				}
+				else if (op == '/')
+				{
+					i = 0 ;
+					v = angka1 ;
 				
-				cout << "Lagi?(Y/N) " ;
-				cin >> kp ;
-			} 
-			while (kp == 'Y' || kp == 'y');
-		}
-		else if (op == '/')
-		{
-			cout << "!!! INI MASIH BELUM SELESAI" << endl ;
-		}
+					while(v >= angka2){
+
+						v -= angka2 ;
+						i++ ;
+
+					}
+					cout << angka1 << " / " << angka2 << " = " << i << endl ;
+				}
+				else
+				{
+					cout << "Maaf" << endl ;
+				}
+
+			cout << "Tekan Y untuk mengulang: " ;
+			cin >> kp ;
+
+		} while (kp == 'Y' || kp == 'y');
+
+		cout << "Program Selesai" << endl ;
 
 		return 0;
 	}
